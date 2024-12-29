ee6df62e66f8d1cfd30ff7cdc2296a437b9b1ea7
khanhtn <khanhtnfptdn@gmail.com>
2024-12-29 22:06:19 +0700

    update profile

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,45 +1,56 @@
-import {FiCloud, FiCode, FiCpu, FiFacebook, FiGithub} from 'react-icons/fi';
+import { FiMonitor, FiFacebook, FiGithub, FiDatabase, FiServer } from "react-icons/fi";
 
 export const AppConfig = {
-  name: "Anh Nhan Nguyen",
-  username: "monokaijs", // should be GitHub username
-  avatar: 'https://i.imgur.com/nqXoDyv.jpeg',
-  verticalAvatar: 'https://i.imgur.com/AD7uyCy.png',
-  introduction: `10+ years experience in software development.\n` +
+  name: "Nhat Khanh Tu",
+  username: "aiempie", // should be GitHub username
+  avatar: "https://i.ibb.co/Jjj5sK0/IMG-2275.jpg",
+  verticalAvatar: "https://i.ibb.co/Jjj5sK0/IMG-2275.jpg",
+  introduction:
+    `1+ years experience in software development.\n` +
     `Working in scalable and high-performance technology systems.\n` +
-    `Web Development, Mobile Development, DevOps & more.`,
-  subscriptions: [{
-    name: "Hire Me",
-    price: "At any price",
-    preferred: true,
-  }, {
-    name: "Freelance",
-    price: "From $100"
-  }],
-  socialLinks: [{
-    name: 'GitHub',
-    url: 'https://github.com/monokaijs',
-    icon: FiGithub
-  }, {
-    name: 'Facebook',
-    url: 'https://facebook.com/delimister',
-    icon: FiFacebook
-  }],
-  expertises: [{
-    name: 'Web Development',
-    icon: FiCode,
-    description: 'React, Next.js, Node.js, Express, Nest.js, Golang, PHP, Python, SQL, MongoDB, GraphQL, REST API, C#, Rust etc.'
-  }, {
-    name: 'Infrastructure',
-    icon: FiCloud,
-    description: 'AWS, GCP, Azure, Kubernetes, Docker, etc.'
-  }, {
-    name: 'IoT Development',
-    icon: FiCpu,
-    description: 'ESP32, ESP8266, Arduino, etc.'
-  }],
+    `Backend Development, Frontend Development & more.`,
+  subscriptions: [
+    {
+      name: "Hire Me",
+      price: "At any price",
+      preferred: true,
+    },
+    {
+      name: "Freelance",
+      price: "From $10",
+    },
+  ],
+  socialLinks: [
+    {
+      name: "GitHub",
+      url: "https://github.com/aiempie",
+      icon: FiGithub,
+    },
+    {
+      name: "Facebook",
+      url: "https://facebook.com/piepie1203",
+      icon: FiFacebook,
+    },
+  ],
+  expertises: [
+    {
+      name: "Frontend Development",
+      icon: FiMonitor,
+      description: "HTML/CSS, JavaScript, Bootstrap, React, Next.js, etc.",
+    },
+    {
+      name: "Backend Development",
+      icon: FiServer,
+      description: "Java/Spring, C#/ASP.Net, Express, Nest.js, OutSystems, Docker, etc.",
+    },
+    {
+      name: "Data Base",
+      icon: FiDatabase,
+      description: "Postgres, MySQL, MongoDB, etc.",
+    },
+  ],
   analytics: {
-    gaId: 'G-NF39CSC1T1',
+    gaId: "G-NF39CSC1T1",
   },
   giscusEnabled: true,
-}
+};
